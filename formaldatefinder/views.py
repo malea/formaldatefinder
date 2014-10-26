5b08fa3fb4a0b05d2cf0c28f5e5c901895d7af44
Joe Pringle <explodingp@gmail.com>
2014-10-25 21:08:47 -0400

    remove comment

--- formaldatefinder/views.py
+++ formaldatefinder/views.py
@@ -4,7 +4,6 @@ from os import getenv
 
 def index(request):
     context = {
-        #'fb_app_id': '606066119471642'
         'fb_app_id': getenv('FB_APP_ID')
     }
     return render(request, 'index.html', context)
