de3ef7796491c79b5915eeaa4e14758014677e49
Joe Pringle <explodingp@gmail.com>
2014-11-15 17:38:51 -0500

    add tests

--- formaldatefinder/models.py
+++ formaldatefinder/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.forms import ModelForm
+from datetime import date
 
-# Create your models here.
 class Event(models.Model):
     event_name = models.CharField(max_length=144)
     event_date = models.DateField()
     sponsor = models.CharField(max_length=144)
     location = models.CharField(max_length=144)
+
+    def is_upcoming(self):
+        return self.event_date > date.today()
 
 class EventForm(ModelForm):
     class Meta:
--- formaldatefinder/tests.py
+++ formaldatefinder/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from formaldatefinder.models import Event
+from datetime import date
 
-# Create your tests here.
+class EventTestCase(TestCase):
+
+    def setUp(self):
+        """ 
+        Create Event objects to use with testing. The first event has 
+        already happened, and the second Event is upcoming. 
+        """
+        Event.objects.create(
+                event_name = "Black Diamond",
+                event_date = date(2001, 9, 11),
+                sponsor = "FIJI", 
+                location = "Electric Ave."
+                )
+
+        Event.objects.create(
+                event_name = "WhyYouMufasa",
+                event_date = date(3005, 1, 1),
+                sponsor = "Glover, Donald", 
+                location = "Mars"
+                )
+
+    def test_upcoming(self):
+        """
+        The old event is not upcoming. 
+        The upcoming event is upcoming.
+        """
+        old_event = Event.objects.get(id=1)
+        upcoming_event = Event.objects.get(id=2)
+
+        self.assertFalse(old_event.is_upcoming())
+        self.assertTrue(upcoming_event.is_upcoming())
+
+
+
