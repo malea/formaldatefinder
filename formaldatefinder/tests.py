0b93db99afe0c60a8a2ecb5c8724f0f74c697c11
Joe Pringle <explodingp@gmail.com>
2014-11-17 16:17:21 -0500

    autopep8 EVERYTHING

--- formaldatefinder/tests.py
+++ formaldatefinder/tests.py
@@ -2,30 +2,31 @@ from django.test import TestCase
 from formaldatefinder.models import Event
 from datetime import date
 
+
 class EventTestCase(TestCase):
 
     def setUp(self):
-        """ 
-        Create Event objects to use with testing. The first event has 
-        already happened, and the second Event is upcoming. 
+        """
+        Create Event objects to use with testing. The first event has
+        already happened, and the second Event is upcoming.
         """
         Event.objects.create(
-                event_name = "Black Diamond",
-                event_date = date(2001, 9, 11),
-                sponsor = "FIJI", 
-                location = "Electric Ave."
-                )
+            event_name="Black Diamond",
+            event_date=date(2001, 9, 11),
+            sponsor="FIJI",
+            location="Electric Ave."
+        )
 
         Event.objects.create(
-                event_name = "WhyYouMufasa",
-                event_date = date(3005, 1, 1),
-                sponsor = "Glover, Donald", 
-                location = "Mars"
-                )
+            event_name="WhyYouMufasa",
+            event_date=date(3005, 1, 1),
+            sponsor="Glover, Donald",
+            location="Mars"
+        )
 
     def test_upcoming(self):
         """
-        The old event is not upcoming. 
+        The old event is not upcoming.
         The upcoming event is upcoming.
         """
         old_event = Event.objects.get(id=1)
@@ -33,6 +34,3 @@ class EventTestCase(TestCase):
 
         self.assertFalse(old_event.is_upcoming())
         self.assertTrue(upcoming_event.is_upcoming())
-
-
-
